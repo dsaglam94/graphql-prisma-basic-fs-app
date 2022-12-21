55b6c2cef55f9ff3074c764d4ee28fa71ac53970
dsaglam94 <d.saglam@outlook.fr>
2022-12-21 18:43:43 +0300

    removing dummy data & adding query to render data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import { type User } from "./types/types";
+import { useQuery } from "urql";
+import { GetUsersDocument } from "./graphql/generated";
+
 import DisplayUsers from "./components/DisplayUsers";
+
 function App() {
-  const users: User[] = [
-    {
-      name: "Prisma Fan",
-      messages: [
-        {
-          body: "Prisma rocks!!",
-        },
-        {
-          body: "Did I mention I love Prisma?",
-        },
-      ],
-    },
-  ];
+  const [results] = useQuery({
+    query: GetUsersDocument,
+  });
 
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-        {users.map((user, i) => (
+        {results?.data?.users.map((user, i) => (
           <DisplayUsers user={user} key={i} />
         ))}
       </div>
